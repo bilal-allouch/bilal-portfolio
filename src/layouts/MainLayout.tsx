3afa3a5b3aea414da1a0e50644fc96e10b6e8292
bilal-allouch <bilalallouch20@hotmail.com>
2025-12-26 19:06:47 +0100

    style: add spacing under fixed header

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,8 +6,9 @@ interface MainLayoutProps {
 
 export default function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-primary to-secondary">
+    <div className="min-h-screen flex flex-col bg-gradient-to-br from-primary to-secondary pt-20">
       {children}
     </div>
   );
 }
+
